66f42882ce88cef49de414c107d9c18715be4cdb
Matt Ankerson <matt.ankerson@intergen.co.nz>
2017-04-03 08:09:01 +1200

    adjust date range

--- date_range_scraper.py
+++ date_range_scraper.py
@@ -36,7 +36,7 @@ def IterateDateRange(start_date, end_date):
                       % (link, e))
 
 
-start = date(2008, 12, 30)
-end = date(2017, 3, 30)
+start = date(2017, 3, 31)
+end = date(2017, 4, 2)
 
 IterateDateRange(start, end)
